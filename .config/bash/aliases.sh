2b58437940ac320a45a2b5336637b1a598a302cb
Dmitri <orleanski@gmail.com>
2021-03-06 10:48:51 -1100

    aliases in a separate file

---
+++ .config/bash/aliases.sh
@@ -0,0 +1,14 @@
+# Aliases {{{
+alias tmux='tmux -2 -u'
+alias nano='nano -w'
+alias l='ls -alhF --color --group-directories-first'
+alias lc='ls -ahF --color --group-directories-first'
+alias ll='ls -alhF --color | sort -r | less'
+alias l1='ls -a1 --color --group-directories-first'
+alias c='echo "==============================" && echo $TZ && date && echo "==============================" && cal -m -3'
+alias h='history'
+alias df='df -h'
+alias du='du -h'
+alias du1='du -h --max-depth 1'
+alias mountp='mount | column -t'
+#}}}
