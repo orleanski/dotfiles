1817ffa6631c08101f13c8a151c33051810c7652
Dmitri <7391330+vyvox@users.noreply.github.com>
2021-03-05 23:54:29 -1100

    splitting from bashrc

---
+++ .config/bash/functions.sh
@@ -0,0 +1,40 @@
+# Functions {{{
+
+# Create a directory and change into it
+mkcd () { mkdir -p "$@" && cd "$@"; }
+
+# Nice history output
+H() {
+    declare -A HMAP
+    while read line
+    do
+        INDEX=$(echo $line | cut -d' ' -f 1)
+        ENTRY=$(echo $line | cut -d' ' -f 2-)
+        HMAP[$ENTRY]=$INDEX
+    done < <(history | rg "$*" )
+    for ENTRY in "${!HMAP[@]}"
+    do
+        printf "%4s %s\n"  "${HMAP[$ENTRY]}" "$ENTRY"
+    done | sort -n
+}
+
+function lowercase()  # move filenames to lowercase
+{
+    for file ; do
+        filename=${file##*/}
+        case "$filename" in
+        */*) dirname==${file%/*} ;;
+        *) dirname=.;;
+        esac
+        nf=$(echo $filename | tr A-Z a-z)
+        newname="${dirname}/${nf}"
+        if [ "$nf" != "$filename" ]; then
+            mv "$file" "$newname"
+            echo "lowercase: $file --> $newname"
+        else
+            echo "lowercase: $file not changed."
+        fi
+    done
+}
+
+# }}}
