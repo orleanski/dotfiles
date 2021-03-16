a77a4d4dc608b2560f8ca29269489280842cf0a3
Dmitri <orleanski@gmail.com>
2021-03-15 17:33:12 -1100

    separating exports for apps in to a dedicated file

---
+++ .config/bash/apps.sh
@@ -0,0 +1,20 @@
+# Apps {{{
+#================================================
+export VIMINIT="source ${XDG_CONFIG_HOME}/vim/vimrc"
+
+#export LESSHISTFILE=-												# No less history (~/.lesshst file)
+# lesskey is binary file generated with lesskey from ~/.lesskey,
+# then copy, also can be uses as /etc/sysless
+export LESSKEY="${XDG_CONFIG_HOME}/less/lesskey"
+
+# check if directory doesn't exist, then create it
+[[ -d ${XDG_DATA_HOME}/less ]] || mkdir -p ${XDG_DATA_HOME}/less
+export LESSHISTFILE="${XDG_DATA_HOME}/less/history"
+
+export GNUPGHOME=${XDG_CONFIG_HOME}/gnupg
+
+export INPUTRC=${XDG_CONFIG_HOME}/readline/inputrc
+
+export MYSQL_HISTFILE=${XDG_DATA_HOME}/mysql_history
+#}}}
+
