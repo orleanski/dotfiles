a50c712b9726e2a32266f469c3fdaf4e43dfad11
Dmitri <7391330+orleanski@users.noreply.github.com>
2021-03-24 18:24:07 -1100

    redone

--- setup.sh
+++ setup.sh
@@ -5,26 +5,54 @@
 
 echo "       Installing dotfiles, it will be quick!"
 echo ""
-cd ~/
+
+cd "$HOME"
+
 echo "Moving .config to .config.bak, can be deleted afterwards"
 [[ -d .config ]] && mv .config .config.bak
+
 echo "Obtaining github archive with config files"
-wget -q https://github.com/orleanski/dotfiles/archive/master.zip
-echo "Extracting only .config dir and subdirs ... see below"
-unzip master.zip 'dotfiles-master/.config/**/*' -d ./
+wget -nc --config=/etc/wgetrc "https://github.com/orleanski/dotfiles/archive/master.zip"
+
+echo "Unzipping archive"
+unzip  master.zip  -d "$HOME/"
+
 echo "Moving .config dir to the homedir"
-mv dotfiles-master/.config/ ./
-echo "Updating current state of the users user-dirs.dir"
-xdg-user-dirs-update
+mv dotfiles-master/.config/  $HOME/
+mv dotfiles-master/Makefile $HOME/
+
+
 echo " .... cleaning up now .... "
+
 echo "Deleting temp unzipped dir"
-rmdir dotfiles-master/
-#echo "Checking and removing master.zip"
-#[[ -f master.zip ]] && rm master.zip
-echo "keep master.zip for now, some files from there you might need later"
+[[ -d dotfiles-master ]] && rm -rf dotfiles-master
+
+echo "Checking and removing master.zip"
+[[ -f master.zip ]] && rm master.zip
+
 echo "Checking and removing .wget-hsts"
 [[ -f .wget-hsts ]] && rm .wget-hsts
+
 echo "Checking and removing .bash_history"
 [[ -f .bash_history ]] && rm .bash_history
+
+echo "Creating homedir structure, may ways"
+[ -d "$HOME/Desktop" ] || mkdir -p "$HOME/Desktop"
+[ -d "$HOME/Documents/Templates" ] || mkdir -p "$HOME/Documents/Templates"
+[ -d "$HOME/Downloads" ] || mkdir "$HOME/Downloads"
+[ -d "$HOME/Media/Books" ] || mkdir -p "$HOME/Media/Books"
+[ -d "$HOME/Media/Music" ] || mkdir "$HOME/Media/Music"
+[ -d "$HOME/Media/Picture/Backgrounds" ] || mkdir -p "$HOME/Media/Pictures/Backgrounds"
+[ -d "$HOME/Media/Picture/Screenshots" ] || mkdir -p "$HOME/Media/Pictures/Screenshots"
+[ -d "$HOME/Media/Videos" ] || mkdir "$HOME/Media/Videos"
+[ -d "$HOME/Projects" ] || mkdir "$HOME/Projects"
+[ -d "$HOME/Public" ] || mkdir "$HOME/Public"
+
+echo "Updating current state of the users user-dirs.dir"
+xdg-user-dirs-update --force
+
+echo "Changing permissions on 700 to .cache .config and .data"
+chmod 700 ${XDG_CACHE_HOME} ${XDG_CONFIG_HOME} ${XDG_DATA_HOME}
+
 echo ""
 echo "       Done "
