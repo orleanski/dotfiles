5dd61eae70d22372057a12787ae45933045f7568
Dmitri <orleanski@gmail.com>
2021-05-07 10:10:07 -1100

    notification about post-install steps

--- setup.sh
+++ setup.sh
@@ -62,3 +62,6 @@ chmod 700 ${XDG_CACHE_HOME} ${XDG_CONFIG_HOME} ${XDG_DATA_HOME}
 
 echo ""
 echo "       Done "
+echo "please add xdg.sh to /etc/profile.d/"
+echo "please emerge x11-misc/xdg-user-dirs"
+echo "please emerge dev-vcs/git for vimplug"
