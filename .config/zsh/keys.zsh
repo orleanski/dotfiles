31ce866fe9a32ed7ff6285ef31394bf5642d41e9
Dmitri Orleanski <orleanski@gmail.com>
2022-05-19 21:15:53 -1100

    home, end, insert, delete bs keys are working as intended

--- .config/zsh/keys.zsh
+++ .config/zsh/keys.zsh
@@ -18,13 +18,40 @@
 # }}}
 # Custom {{{
 #================================================
+    # Up                   '^[[A'
+    # Down                 '^[[B'
+    # Right                '^[[C'
+    # Left                 '^[[D'
+    # Home                 '^[[H'
+    # End                  '^[[F'
+    # Insert               '^[[2~'
+    # Delete               '^[[3~'
+    # PageUp               '^[[5~'
+    # PageDown             '^[[6~'
+    # Backspace            '^?'
+
+# HOME key
+bindkey '\e[1~' beginning-of-line
+
+# END key
+bindkey '\e[4~' end-of-line
+
+# DELETE key
+bindkey '\e[3~' delete-char
+
+# INSERT key
+bindkey '^[[2~'  overwrite-mode
+
+# BACKSPACE key
+bindkey '^?' backward-delete-char
+
   # Allow to move to beginning/end of line
-  bindkey '^a' beginning-of-line
-  bindkey '^e' end-of-line
+  # bindkey '^a' beginning-of-line
+  # bindkey '^e' end-of-line
 
   # Allow to move to backward/forward of word
-  bindkey '^b' backward-word
-  bindkey '^f' forward-word
+  # bindkey '^b' backward-word
+  # bindkey '^f' forward-word
 
   ## Page UP and Page Down through History
 autoload -Uz history-search-end
